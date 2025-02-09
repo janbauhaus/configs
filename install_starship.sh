2f73dc1babce35b0d13443b66feeadf16bd585c7
Jan Bauhaus <61849552+janbauhaus@users.noreply.github.com>
2025-02-09 16:57:25 +0100

    Update install_starship.sh

--- install_starship.sh
+++ install_starship.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 curl -sS https://starship.rs/install.sh | sh
-eval "$(starship init zsh)"
+echo 'eval "$(starship init zsh)"' >> .zshrc
 curl --create-dirs --output ~/.config/starship.toml https://github.com/janbauhaus/configs/blob/master/starship.toml
