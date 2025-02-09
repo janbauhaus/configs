06dcd8a2a2af42ad0d03e11a6d39462074e05fa4
Jan Bauhaus <61849552+janbauhaus@users.noreply.github.com>
2025-02-09 17:10:52 +0100

    Update install_nvim.sh

--- install_nvim.sh
+++ install_nvim.sh
@@ -6,3 +6,7 @@ cd neovim
 git checkout stable
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+curl --create-dirs --output ~/.config/nvim/init.vim https://raw.githubusercontent.com/janbauhaus/configs/refs/heads/master/nvim/init.vim
