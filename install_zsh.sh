792f3f5a70b15a06a10f5a57be731b7318d5407c
Jan Bauhaus <61849552+janbauhaus@users.noreply.github.com>
2025-09-02 15:26:52 +0200

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -6,3 +6,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sed -i 's|plugins=(git)|plugins=(git z zsh-syntax-highlighting zsh-autosuggestions)|g' .zshrc
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+sed -i 's|ZSH_THEME="robbyrussell"|ZSH_THEME="powerlevel10k/powerlevel10k"|g' .zshrc
